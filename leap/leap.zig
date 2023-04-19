ae57fb7ac3b5162d973ca3b56ed3df6fcd063fcb
Sacha Muszlak <sacha@muszlak.com>
2023-04-19 22:37:07 +0200

    updated

--- leap/leap.zig
+++ leap/leap.zig
@@ -1,6 +1,29 @@
+const std = @import("std");
+//const math = @import("math");
+
 pub fn isLeapYear(year: u32) bool {
-    _ = year;
-    if (year % 4 == 0) {
+    // on every year that is evenly divisible by 4
+    const leapyear = 4;
+    const mod = year % leapyear;
+    // std.debug.print("year {} modulo {} is {}", year leapyear mod);
+    if (mod == 0) {
+        const unleapyear = 100;
+        const unleap = year % unleapyear;
+
+        // except every year that is evenly divisible by 100
+        if (unleap == 0) {
+            // unless the year is also evenly divisible by 400
+            const ununleapyear = 400;
+            const ununleap = year % ununleapyear;
+            if (ununleap == 0) {
+                return true;
+            } else {
+                return false;
+            }
+            return true;
+        }
         return true;
     }
+    return false;
 }
+
